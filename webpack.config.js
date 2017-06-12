066808782b952b299d4f5c32695e3bf6a49f0377
Jeff <jeff.burkholder@gmail.com>
2017-06-12 11:25:47 -0500

    update package versions, webpack2 config

--- webpack.config.js
+++ webpack.config.js
@@ -14,21 +14,21 @@ module.exports = {
     ]
   },
   output: {
-    path: './dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js'
   },
   module: {
     loaders: [
-      {test: /\.component.ts$/, loader: 'ts!angular2-template'},
-      {test: /\.ts$/, exclude: /\.component.ts$/, loader: 'ts'},
-      {test: /\.html$/, loader: 'raw'},
-      {test: /\.css$/, include: path.resolve('src/app'), loader: 'raw'},
-      {test: /\.css$/, exclude: path.resolve('src/app'), loader: ExtractTextPlugin.extract('style', 'css')},
-      {test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file?name=fonts/[name].[ext]'}
+      {test: /\.component.ts$/, loader: 'ts-loader!angular2-template-loader'},
+      {test: /\.ts$/, exclude: /\.component.ts$/, loader: 'ts-loader'},
+      {test: /\.html$/, loader: 'raw-loader'},
+      {test: /\.css$/, include: path.resolve('src/app'), loader: 'raw-loader'},
+      {test: /\.css$/, exclude: path.resolve('src/app'), loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })},
+      {test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file-loader?name=fonts/[name].[ext]'}
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.ts', '.html', '.css']
+    extensions: ['.js', '.ts', '.html', '.css']
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
